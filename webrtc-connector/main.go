1c140d755f7ada74a084b8b63cae6624d188546f
Ato Araki <ato.araki@gmail.com>
2020-06-18 22:04:08 +0900

    add README

--- webrtc-connector/main.go
+++ webrtc-connector/main.go
@@ -63,14 +63,6 @@ func runSession(ctx context.Context) error {
 
 	log.Println("Waiting for RTP Packets on port 5004, please run GStreamer or ffmpeg now")
 
-	// example:
-	//  test src
-	//   $ gst-launch-1.0 videotestsrc ! 'video/x-raw, width=320, height=240' ! videoconvert ! video/x-raw,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5004
-	//  mjpeg http src
-	//   $ gst-launch-1.0 souphttpsrc location=http://127.0.0.1:8080/mjpeg is-live=true ! multipartdemux ! image/jpeg,width=640,height=320,framerate=10/1 ! jpegdec ! videorate ! video/x-raw,framerate=5/1 ! videoconvert ! video/x-raw,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5004
-	//  uvc camera src
-	//   $ gst-launch-1.0 autovideosrc name=src0 ! video/x-raw,width=640,height=480 ! videoconvert ! video/x-raw,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5004
-
 	// Listen for a single RTP Packet, we need this to determine the SSRC
 	inboundRTPPacket := make([]byte, 4096) // UDP MTU
 	n, _, err := listener.ReadFromUDP(inboundRTPPacket)
