05214c483cda5407ec151f71891d90b5c060e239
Ato Araki <ato.araki@gmail.com>
2025-03-06 14:11:35 +0900

    webrtc-connector: upgrade pion/webrtc

--- webrtc-connector/go.mod
+++ webrtc-connector/go.mod
@@ -1,9 +1,33 @@
 module github.com/atotto/mibot/webrtc-connector
 
-go 1.14
+go 1.23.0
+
+toolchain go1.24.0
 
 require (
-	github.com/atotto/webrtc-sdp-exchanger/exchange v0.1.0
-	github.com/pion/rtp v1.5.4
-	github.com/pion/webrtc/v2 v2.2.14
+	github.com/atotto/webrtc-sdp-exchanger/exchange v0.2.0
+	github.com/pion/webrtc/v4 v4.0.13
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pion/datachannel v1.5.10 // indirect
+	github.com/pion/dtls/v3 v3.0.4 // indirect
+	github.com/pion/ice/v4 v4.0.7 // indirect
+	github.com/pion/interceptor v0.1.37 // indirect
+	github.com/pion/logging v0.2.3 // indirect
+	github.com/pion/mdns/v2 v2.0.7 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.15 // indirect
+	github.com/pion/rtp v1.8.12 // indirect
+	github.com/pion/sctp v1.8.37 // indirect
+	github.com/pion/sdp/v3 v3.0.10 // indirect
+	github.com/pion/srtp/v3 v3.0.4 // indirect
+	github.com/pion/stun/v3 v3.0.0 // indirect
+	github.com/pion/transport/v3 v3.0.7 // indirect
+	github.com/pion/turn/v4 v4.0.0 // indirect
+	github.com/wlynxg/anet v0.0.5 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
--- webrtc-connector/main.go
+++ webrtc-connector/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -14,14 +15,13 @@ import (
 	"time"
 
 	"github.com/atotto/webrtc-sdp-exchanger/exchange"
-	"github.com/pion/rtp"
-	"github.com/pion/webrtc/v2"
+	"github.com/pion/webrtc/v4"
 )
 
 var (
 	sessionID      = flag.String("session", "test", "Session ID")
 	commandPath    = flag.String("c", "../mibot", "execution command path")
-	commandArgs    = flag.String("arg", "", "exacution command arg")
+	commandArgs    = flag.String("arg", "", "execution command arg")
 	rtpPort        = flag.Int("rtpPort", 5004, "RTP Port")
 	videoCodecName = flag.String("codec", "VP8", "Codec Type")
 )
@@ -61,26 +61,16 @@ func runSession(ctx context.Context) error {
 	defer cancel()
 
 	// Open a UDP Listener for RTP Packets on port `rtpPort`
-	//listener, err := net.ListenPacket("udp", fmt.Sprintf("%s:%d", "127.0.0.1", *rtpPort))
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: *rtpPort})
 	if err != nil {
 		return fmt.Errorf(": %s", err)
 	}
 	defer listener.Close()
 
-	log.Printf("Waiting for RTP Packets on port %s, please run GStreamer or ffmpeg now", listener.LocalAddr().String())
-
-	// Listen for a single RTP Packet, we need this to determine the SSRC
-	inboundRTPPacket := make([]byte, 4096) // UDP MTU
-	n, _, err := listener.ReadFromUDP(inboundRTPPacket)
-	if err != nil {
-		return fmt.Errorf("read RTP packets: %s", err)
-	}
-
-	// Unmarshal the incoming packet
-	packet := &rtp.Packet{}
-	if err = packet.Unmarshal(inboundRTPPacket[:n]); err != nil {
-		return fmt.Errorf("unmarshal RTP packet: %s", err)
+	// Increase the UDP receive buffer size
+	// Default UDP buffer sizes vary on different operating systems
+	if err := listener.SetReadBuffer(300 * 1024); err != nil {
+		return fmt.Errorf("failed to set the buffer size: %s", err)
 	}
 
 	log.Print("getting new offer...")
@@ -93,49 +83,56 @@ func runSession(ctx context.Context) error {
 	var settingEngine = webrtc.SettingEngine{}
 	settingEngine.SetNetworkTypes([]webrtc.NetworkType{webrtc.NetworkTypeUDP4})
 
-	// We make our own mediaEngine so we can place the sender's codecs in it.  This because we must use the
-	// dynamic media type from the sender in our answer. This is not required if we are the offerer
-	mediaEngine := webrtc.MediaEngine{}
-	if err := mediaEngine.PopulateFromSDP(*offer); err != nil {
-		return fmt.Errorf("SDP: %s", err)
-	}
+	videoMimeType := "video/" + *videoCodecName
 
-	// Search for `videoCodecName` Payload type. If the offer doesn't support specified codec exit
-	// since they won't be able to decode anything we send them
-	var payloadType uint8
-	for _, videoCodec := range mediaEngine.GetCodecsByKind(webrtc.RTPCodecTypeVideo) {
-		if videoCodec.Name == *videoCodecName {
-			payloadType = videoCodec.PayloadType
-			break
-		}
-	}
-	if payloadType == 0 {
-		log.Print("Remote peer does not support VP8")
-		return nil // retry
+	mediaEngine := &webrtc.MediaEngine{}
+
+	// Set up the codecs
+	if err := mediaEngine.RegisterCodec(webrtc.RTPCodecParameters{
+		RTPCodecCapability: webrtc.RTPCodecCapability{
+			MimeType: videoMimeType, ClockRate: 90000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil,
+		},
+	}, webrtc.RTPCodecTypeVideo); err != nil {
+		return fmt.Errorf("register video codec: %s", err)
 	}
 
 	// Create a new RTCPeerConnection
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(mediaEngine), webrtc.WithSettingEngine(settingEngine))
-	peerConnection, err := api.NewPeerConnection(config)
+	//api := webrtc.NewAPI(webrtc.WithMediaEngine(mediaEngine), webrtc.WithSettingEngine(settingEngine))
+	//peerConnection, err := api.NewPeerConnection(config)
+	peerConnection, err := webrtc.NewPeerConnection(config)
 	if err != nil {
 		return fmt.Errorf("new perr connection: %s", err)
 	}
 
-	// Create a video track, using the same SSRC as the incoming RTP Packet
-	videoTrack, err := peerConnection.NewTrack(payloadType, packet.SSRC, "video", "pion")
+	// Create a video track
+	videoTrack, err := webrtc.NewTrackLocalStaticRTP(
+		webrtc.RTPCodecCapability{MimeType: videoMimeType}, "video", "pion",
+	)
 	if err != nil {
 		return fmt.Errorf("new video track: %s", err)
 	}
-	if _, err = peerConnection.AddTrack(videoTrack); err != nil {
+	rtpSender, err := peerConnection.AddTrack(videoTrack)
+	if err != nil {
 		return fmt.Errorf("add video track: %s", err)
 	}
+
+	// Read incoming RTCP packets
+	// Before these packets are returned they are processed by interceptors. For things
+	// like NACK this needs to be called.
+	go func() {
+		rtcpBuf := make([]byte, 1500)
+		for {
+			if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+				return
+			}
+		}
+	}()
 
 	// Set the handler for ICE connection state
 	// This will notify you when the peer has connected/disconnected
 	peerConnection.OnICEConnectionStateChange(func(state webrtc.ICEConnectionState) {
 		log.Printf("ICE Connection State has changed %s \n", state.String())
 		if state == webrtc.ICEConnectionStateFailed || state == webrtc.ICEConnectionStateDisconnected || state == webrtc.ICEConnectionStateClosed {
-			listener.Close()
 			peerConnection.Close()
 			cancel()
 		}
@@ -207,19 +204,31 @@ func runSession(ctx context.Context) error {
 		return fmt.Errorf("set remote session description: %s", err)
 	}
 
+	log.Println("create answer")
+
 	// Create answer
 	answer, err := peerConnection.CreateAnswer(nil)
 	if err != nil {
 		return fmt.Errorf("create answer: %s", err)
 	}
 
+	// Create channel that is blocked until ICE Gathering is complete
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+
 	// Sets the LocalDescription, and starts our UDP listeners
 	if err = peerConnection.SetLocalDescription(answer); err != nil {
 		return fmt.Errorf("set local description: %s", err)
 	}
 
+	// Block until ICE Gathering is complete, disabling trickle ICE
+	// we do this because we only can exchange one signaling message
+	// in a production application you should exchange ICE Candidates via OnICECandidate
+	<-gatherComplete
+
+	log.Println("send answer")
+
 	// Send the answer
-	if err := exchange.CreateSession(ctx, &answer, *sessionID); err != nil {
+	if err := exchange.CreateSession(ctx, peerConnection.LocalDescription(), *sessionID); err != nil {
 		return fmt.Errorf("send answer: %s", err)
 	}
 
@@ -235,6 +244,7 @@ func runSession(ctx context.Context) error {
 	}
 
 	// Read RTP packets forever and send them to the WebRTC Client
+	inboundRTPPacket := make([]byte, 1600) // UDP MTU
 	for {
 		n, _, err := listener.ReadFrom(inboundRTPPacket)
 		if err != nil {
@@ -244,14 +254,8 @@ func runSession(ctx context.Context) error {
 			return fmt.Errorf("error during read: %s", err)
 		}
 
-		packet := &rtp.Packet{}
-		if err := packet.Unmarshal(inboundRTPPacket[:n]); err != nil {
-			return fmt.Errorf("unmarshal RTP packet: %s", err)
-		}
-		packet.Header.PayloadType = payloadType
-
-		if err := videoTrack.WriteRTP(packet); err != nil {
-			if err == io.ErrClosedPipe {
+		if _, err := videoTrack.Write(inboundRTPPacket[:n]); err != nil {
+			if errors.Is(err, io.ErrClosedPipe) {
 				return nil
 			}
 			return fmt.Errorf("write video: %s", err)
